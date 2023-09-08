235f3bef25e4bdd144a86d2de2bab0d9363c9619
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2023-09-08 09:25:16 -0400

    1 page pdf

---
+++ main.py
@@ -0,0 +1,16 @@
+#https://pypi.org/project/PyPDF2/
+#https://pypdf2.readthedocs.io/en/stable/
+
+import PyPDF2 
+import pathlib 
+
+filepath = pathlib.Path(__file__).parent / "documents\dummy.pdf"
+
+with open(filepath, 'rb') as file:
+    reader = PyPDF2.PdfReader(file)
+    # to get the number of pages use len(reder.pages)
+    print(len(reader.pages))
+
+
+
+
